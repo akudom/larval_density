1020ae15604c973c2862596a5d841467b0e6ae15
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-01 14:57:01 +0100

    Basic data tidying and plotting

--- scripts/1_pupation_duration.R
+++ scripts/1_pupation_duration.R
@@ -7,6 +7,14 @@ library(readxl)
 library(janitor)
 
 input_data <- read_excel('data/raw_data/Data_density dependent.xlsx') %>%
-  clean_names()
+  clean_names() %>%
+  mutate(larval_density = gsub('L', '', larval_density),
+         larval_density = as.numeric(larval_density),
+         period_of_adult_pupation_day = as.numeric(period_of_adult_pupation_day),
+         species = tolower(species))
 str(input_data)
 
+ggplot(input_data, aes(x = larval_density, y = period_of_pupation_day)) +
+  geom_point() +
+    theme_bw() +
+  facet_wrap(. ~ species)
