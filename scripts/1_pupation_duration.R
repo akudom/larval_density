bc507d4d3a489156cf6222567d0ac5ddec3c187a
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-04 11:10:40 +0100

    Added a load of comments, formatted data to make a quick violinplot possible

--- scripts/1_pupation_duration.R
+++ scripts/1_pupation_duration.R
@@ -6,15 +6,32 @@ library(lme4)
 library(readxl)
 library(janitor)
 
-input_data <- read_excel('data/raw_data/Data_density dependent.xlsx') %>%
+# read in data
+input_data <- read_excel('data/raw_data/Data_density dependent.xlsx') %>% 
+  # remove spaces etc from column names
   clean_names() %>%
-  mutate(larval_density = gsub('L', '', larval_density),
+  mutate(# remove the character 'L' from larval_density
+    larval_density = gsub('L', '', larval_density),
+         # convert larval_density to numeric 
          larval_density = as.numeric(larval_density),
+         # replace the string 'N/A' with NA values in period_of_adult_pupation_day
+         period_of_adult_pupation_day = gsub('N/A', NA, period_of_adult_pupation_day),
+         # convert period_of_adult_pupation_day from character to numeric
          period_of_adult_pupation_day = as.numeric(period_of_adult_pupation_day),
+         # make species entirely lowercase, as it's currently mixed 
          species = tolower(species))
 str(input_data)
 
-ggplot(input_data, aes(x = larval_density, y = period_of_pupation_day)) +
-  geom_point() +
+# make a simple pair of violin plots to show summary stats
+violins <- input_data %>% 
+  pivot_longer(c(period_of_pupation_day, period_of_adult_pupation_day), 
+               names_to = 'stat_type', values_to = 'value') %>%
+  ggplot(., aes(x = species, y = value, fill = species)) +
+  geom_violin() +
     theme_bw() +
-  facet_wrap(. ~ species)
+  theme(legend.position = 'bottom') +
+  scale_fill_viridis_d() +
+  facet_grid(stat_type ~ larval_density, 
+             switch = 'y')
+ggsave(filename = 'figures/violinplot.pdf',
+       violins)
