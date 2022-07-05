56d40f408bac1b12a2ddcaf77e605b9ac70c8ca2
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-05 14:42:31 +0100

    Calculated survival rates

--- scripts/2_larval_mortality.R
+++ scripts/2_larval_mortality.R
@@ -8,3 +8,21 @@
 
 library(tidyverse)
 larval_df <- read_csv('data/processed_data/density_and_growth.csv')
+
+str(larval_df)
+
+survival_df <- larval_df %>%
+  # remove any rows where theres an NA on the eclosion
+  # date, that way we only have rows for larvae that
+  # successfully eclosed (i.e. survived to adulthood)
+  filter(!is.na(period_of_eclosion_day)) %>%
+  # make a column per grouping variable stating how
+  # many survived
+  group_by(date_of_experiment, larval_density,
+           replicate_no, species) %>%
+  summarise(n_survived = n()) %>%
+  # make a column showing how many died
+  mutate(n_died = larval_density - n_survived,
+         proportion_survival = n_survived / larval_density)
+            
+            
