fd39b543f5129a0a8fc4c45e9e519a8056c86cf9
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-05 13:58:05 +0100

    Corrected poorly-chosen model parameters, tidied data a bit

--- scripts/1_pupation_duration.R
+++ scripts/1_pupation_duration.R
@@ -13,15 +13,17 @@ input_data <- read_excel('data/raw_data/Data_density dependent.xlsx') %>%
   clean_names() %>%
   rename(period_of_eclosion_day = period_of_adult_pupation_day) %>%
   mutate(# remove the character 'L' from larval_density
-    larval_density = gsub('L', '', larval_density),
-         # convert larval_density to numeric 
-         larval_density = as.numeric(larval_density),
+        larval_density = gsub('L', '', larval_density),
+        larval_density = forcats::as_factor(larval_density),
          # replace the string 'N/A' with NA values in period_of_adult_pupation_day
-         period_of_eclosion_day = gsub('N/A', NA, period_of_eclosion_day),
-         # convert period_of_adult_pupation_day from character to numeric
-         period_of_eclosion_day = as.numeric(period_of_eclosion_day),
-         # make species entirely lowercase, as it's currently mixed 
-         species = tolower(species))
+        period_of_eclosion_day = gsub('N/A', NA, period_of_eclosion_day),
+        # convert period_of_adult_pupation_day from character to numeric
+        period_of_eclosion_day = as.numeric(period_of_eclosion_day),
+        # make species entirely lowercase, as it's currently mixed 
+        species = tolower(species),
+    sample_replicate = paste(date_of_experiment, replicate_no, sep = '_'),
+    replicate_no = as.character(replicate_no),
+    date_of_experiment = as.character(date_of_experiment))
 str(input_data)
 
 
@@ -39,7 +41,7 @@ long_data <- input_data %>%
 
 # make a simple pair of violin plots to show summary stats
 violins <- ggplot(long_data, 
-                  aes(x = factor(larval_density), y = value, colour = Species))+ 
+                  aes(x = larval_density, y = value, colour = Species))+ 
   geom_violin() + 
   facet_wrap(. ~ stat_type, strip.position = 'left') + 
   theme_bw() +
@@ -73,12 +75,20 @@ ggsave(filename = 'figures/boxplot.pdf', box_plot)
 # Model -------------------------------------------------------------------
 
 
-pupation_model <- lmer(formula = period_of_pupation_day ~ 1 + (1|species) + 
-                         (1|larval_density), data = input_data)
+pupation_model_kisumu <- glmer(formula = period_of_pupation_day ~ larval_density +
+                          (1|sample_replicate),
+                        data = filter(input_data, species == 'kisumu'),
+                        family = 'poisson')
 
-summary(pupation_model)
+summary(pupation_model_kisumu)
+qqnorm(resid(pupation_model_kisumu)) 
+qqline(resid(pupation_model_kisumu), col = "red")
 
-eclosion_model <- lmer(formula = period_of_eclosion_day ~ 1 + (1|species) + 
-                         (1|larval_density), data = input_data)
+eclosion_model_kisumu <- glmer(formula = period_of_eclosion_day ~ larval_density +
+                                 (1|sample_replicate),
+                               data = filter(input_data, species == 'kisumu'),
+                               family = 'poisson')
 
-summary(eclosion_model)
+summary(eclosion_model_kisumu)
+qqnorm(resid(eclosion_model_kisumu)) 
+qqline(resid(eclosion_model_kisumu), col = "red")
