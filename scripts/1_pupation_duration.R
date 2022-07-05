aaf2c75773485fd03e29bd855cfd16f17de33efd
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-05 14:32:09 +0100

    Added brief header

--- scripts/1_pupation_duration.R
+++ scripts/1_pupation_duration.R
@@ -1,3 +1,8 @@
+# code by Dave Hemprich-Bennett and Talya Hackett, 
+# working on data by Andreas Kudom
+# analyses the effect of Anopheles gambiae larval density
+# on larval growth rates
+
 
 # Setup -------------------------------------------------------------------
 
